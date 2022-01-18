26a180df7fcb5f66d9907b4618c959d0150822b3
mario <mario@mario.mario>
2022-01-18 15:11:58 +0100

    i

--- src/it/etlabora/provacom/servlet/ProvaServlet.java
+++ src/it/etlabora/provacom/servlet/ProvaServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ProvaServlet extends HttpServlet{
 
+	// commento utilissimo
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		resp.setContentType("text/html");
