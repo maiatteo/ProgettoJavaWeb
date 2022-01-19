a8f38336aabec8adfa7a518801a2fce5e828ab3b
Matteo <matteomaialetti@gmail.com>
2022-01-19 12:08:37 +0100

    Fixed UtentiService

--- src/it/etlabora/progettojavaweb/service/UtentiService.java
+++ src/it/etlabora/progettojavaweb/service/UtentiService.java
@@ -2,28 +2,20 @@ package it.etlabora.progettojavaweb.service;
 
 import java.util.List;
 
+import it.etlabora.progettojavaweb.dto.UtentiDto;
+
 public interface UtentiService {
 	
-		/**
-		 * dto.id has to be null (it'll be ignored)
-		 */
 		public UtentiDto create(UtentiDto dto);
 
-		/**
-		 * Id must not be null
-		 */
 		public UtentiDto update(UtentiDto dto);
 
-		/**
-		 * Id must not be null
-		 */
 		public UtentiDto getOne(Long id);
 
-		/**
-		 * Id must not be null
-		 */
 		public void delete(Long id);
 
 		public List<UtentiDto> getAll();
-	}
+		
+		public void login(String username, String password);
+	
 }
