f8c09a2488705220a1373f3276d03547adbac881
mario <mario@mario.mario>
2022-01-19 11:18:42 +0100

    Merge branch 'master' of https://github.com/maiatteo/ProgettoJavaWeb.git

--- src/it/etlabora/Biblioteca/util/package-info.java
+++
@@ -1,1 +0,0 @@
-package it.etlabora.Biblioteca.util;
--- src/it/etlabora/progettojavaweb/util/DbConnection.java
+++ src/it/etlabora/progettojavaweb/util/DbConnection.java
@@ -1,3 +1,4 @@
+//<<<<<<< HEAD
 package it.etlabora.progettojavaweb.util;
 
 import java.sql.Connection;
@@ -6,17 +7,17 @@ import java.sql.DriverManager;
 public class DbConnection {
 
 	public static final String DB_DRIVER = "org.sqlite.JDBC";
-	//                                      com.mysql.jdbc.Driver
-	public static final String DB_NAME = "jdbc:sqlite:C:\\Users\\Studente\\git\\ProgettoJavaWeb\\Biblioteca";
+	//                                      org.sqlite.JDBC
+	public static final String DB_NAME = "jdbc:sqlite:C:\\Users\\Talento\\git\\ProgettoJavaWeb\\Biblioteca";
 	//  Nome preciso =>     jdbc:sqlite:C:\\path\\to\\file.db
-	//public static final String DB_USERNAME = "root";
-	//public static final String DB_PASSWORD = "root";
+	public static final String DB_USERNAME = "root";
+	public static final String DB_PASSWORD = "root";
 
 	public static Connection getConnection() throws Exception {
 		Connection conn = null;
 		Class.forName(DB_DRIVER);
 
-		conn = DriverManager.getConnection(DB_NAME);
+		conn = DriverManager.getConnection(DB_NAME, DB_USERNAME, DB_PASSWORD);
 		return conn;
 	}
 }
