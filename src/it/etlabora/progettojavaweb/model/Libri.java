ec35f9a6ac7accd7468fc52e7cd0331f20adc4b4
Matteo <matteomaialetti@gmail.com>
2022-01-19 15:35:22 +0100

    Libri model

---
+++ src/it/etlabora/progettojavaweb/model/Libri.java
@@ -0,0 +1,70 @@
+package it.etlabora.progettojavaweb.model;
+
+public class Libri {
+	
+	private Integer id;
+	private String titolo;
+	private String casaeditrice;
+	private String autori;
+	private String categoria;
+	private String isbn;
+	private String note;
+	private String stato;
+	private Integer idutente;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public String getCasaeditrice() {
+		return casaeditrice;
+	}
+	public void setCasaeditrice(String casaeditrice) {
+		this.casaeditrice = casaeditrice;
+	}
+	public String getAutori() {
+		return autori;
+	}
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getStato() {
+		return stato;
+	}
+	public void setStato(String stato) {
+		this.stato = stato;
+	}
+	public Integer getIdutente() {
+		return idutente;
+	}
+	public void setIdutente(Integer idutente) {
+		this.idutente = idutente;
+	}
+	
+
+}
