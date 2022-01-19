345cb0babb04bd3a78c538342a9119c2ed137ccd
Matteo <matteomaialetti@gmail.com>
2022-01-19 17:50:41 +0100

    Added Session and check //todo jstl login

--- src/it/etlabora/progettojavaweb/servlet/Login.java
+++ src/it/etlabora/progettojavaweb/servlet/Login.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import it.etlabora.progettojavaweb.dto.UtentiDto;
 import it.etlabora.progettojavaweb.service.UtentiService;
@@ -17,14 +18,16 @@ public class Login extends HttpServlet{
 	UtentiService utenteService = new UtentiServiceImpl();
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		UtentiDto dto = utenteService.login(username, password);
 		boolean check = false;
 		if (dto == null) {
-			//check = true;
-			//req.setAttribute("check", Boolean.valueOf(check));
-			req.getRequestDispatcher("jsp/index.jsp");
+			check = true;
+			session.setAttribute("check", Boolean.valueOf(check));
+			req.getRequestDispatcher("jsp/index.jsp").forward(req, resp);
+			return;
 		}
 		req.setAttribute("username", dto.getUsername());
 		req.getRequestDispatcher("jsp/login.jsp").forward(req, resp);
