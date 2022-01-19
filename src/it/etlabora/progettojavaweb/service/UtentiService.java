0e1bc220b11e18cae3800926dc35ca570edabdcd
mario <mario@mario.mario>
2022-01-19 12:03:48 +0100

    here we go

--- src/it/etlabora/progettojavaweb/service/UtentiService.java
+++ src/it/etlabora/progettojavaweb/service/UtentiService.java
@@ -1,5 +1,29 @@
 package it.etlabora.progettojavaweb.service;
 
-public interface UtentiService {
+import java.util.List;
 
+public interface UtentiService {
+	
+		/**
+		 * dto.id has to be null (it'll be ignored)
+		 */
+		public UtentiDto create(UtentiDto dto);
+
+		/**
+		 * Id must not be null
+		 */
+		public UtentiDto update(UtentiDto dto);
+
+		/**
+		 * Id must not be null
+		 */
+		public UtentiDto getOne(Long id);
+
+		/**
+		 * Id must not be null
+		 */
+		public void delete(Long id);
+
+		public List<UtentiDto> getAll();
+	}
 }
