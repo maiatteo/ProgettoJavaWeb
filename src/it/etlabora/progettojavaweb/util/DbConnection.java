50cd1c04f081150d7cee63e3183df52819240cea
mario <mario@mario.mario>
2022-01-19 11:12:18 +0100

    Va tutto bene, ssshhhh

---
+++ src/it/etlabora/progettojavaweb/util/DbConnection.java
@@ -0,0 +1,22 @@
+package it.etlabora.progettojavaweb.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection {
+
+	public static final String DB_DRIVER = "org.sqlite.JDBC";
+	//                                      com.mysql.jdbc.Driver
+	public static final String DB_NAME = "jdbc:sqlite:C:\\Users\\Studente\\git\\ProgettoJavaWeb\\Biblioteca";
+	//  Nome preciso =>     jdbc:sqlite:C:\\path\\to\\file.db
+	//public static final String DB_USERNAME = "root";
+	//public static final String DB_PASSWORD = "root";
+
+	public static Connection getConnection() throws Exception {
+		Connection conn = null;
+		Class.forName(DB_DRIVER);
+
+		conn = DriverManager.getConnection(DB_NAME);
+		return conn;
+	}
+}
